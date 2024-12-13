33dff1807f28aa0f41c796f9c2f55e6506abda8a
Jake Kendrick <jakekendrick05@gmail.com>
2024-12-13 10:46:58 +0800

    Added new packages to install requirements

--- psmasegmentator/setup.py
+++ psmasegmentator/setup.py
@@ -5,11 +5,21 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "SimpleITK",
-        "nnunet",
-        "numpy",
-        "argparse"
-    ],
+            'torch>=2.1.2',
+            'numpy<2',
+            'SimpleITK',
+            'nibabel>=2.3.0',
+            'tqdm>=4.45.0',
+            'p_tqdm',
+            'xvfbwrapper',
+            'nnunetv2>=2.2.1',
+            'requests==2.27.1;python_version<"3.10"',
+            'requests;python_version>="3.10"',
+            'rt_utils',
+            'dicom2nifti',
+            'pyarrow',
+            'pydicom'
+        ],
     entry_points={
         "console_scripts": [
             "psma-segmentator = psma_segmentator.cli:main"
