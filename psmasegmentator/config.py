d2d1cf2769bac66346976a8abfc23ff428639696
JakeKendrick1 <jakekendrick05@gmail.com>
2025-01-07 14:05:54 +0800

    Added the acquisition of the psma segmentator base directory.

--- psmasegmentator/config.py
+++ psmasegmentator/config.py
@@ -1,14 +1,26 @@
 import os
 from pathlib import Path
-import requests
 
 
-def nnunet_setup()
+def get_psmasegmentator_dir():
+    """
+    Get the path to the psmasegmentator directory, containing the model weights
+    that have been downloaded.
+    """
+    
+    if "PSMA_SEGMENTATOR_DIR" in os.environ:
+        return Path(os.environ["PSMA_SEGMENTATOR_DIR"])
+    else:
+        # Put in a temporary directory for now
+        return Path("/tmp/.psmasegmentator")
+
+def nnunet_setup():
     """
     Set up the required paths to run nnunet.
     """
+    psma_segmentator_dir = get_psmasegmentator_dir()
+    weights_dir = psma_segmentator_dir / "weights"
 
-    weights_dir = ## SET the path to the weights directory
     os.environ["nnUNet_raw"] = str(weights_dir) #Should not be needed for running inference.
     os.environ["nnUNet_preprocessed"] = str(weights_dir) #Should not be needed for running inference.
     os.environ["nnUNet_results"] = str(weights_dir)
