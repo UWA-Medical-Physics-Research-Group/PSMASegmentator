ec5f037df78830694182c0d0da1d7573419b5034
JakeKendrick1 <jakekendrick05gmail.com>
2025-01-17 12:33:17 +0800

    PSMA_Segmentator function created

--- psmasegmentator/python_api.py
+++ psmasegmentator/python_api.py
@@ -3,9 +3,14 @@ import os
 from pathlib import Path
 import SimpleITK as sitk
 import numpy as np
+from psmasegmentator.download_weights import download_fold_weights_via_api
+from psmasegmentator.inference import nnUNet_predict_image
+from psmasegmentator.config import get_psmasegmentator_dir, setup_psmasegmentator
 
 
-def psmasegmentator (input_dir=None, output_dir=None, file_format="dicom", suv_threshold=3):
+def psmasegmentator (pet_dir=None, 
+                     ct_dir = None,
+                     output_dir=None):
     """
     Runs the full PSMA PET segmentation pipeline.
 
@@ -15,9 +20,12 @@ def psmasegmentator (input_dir=None, output_dir=None, file_format="dicom", suv_t
         file_format (str): Input file format, either "dicom" or "nifti". Defaults to "dicom".
         suv_threshold (float): SUV threshold for segmentation refinement. Defaults to 3.
 
-    Returns:
-        dict: Paths to the output segmentation files.
-
     """
-    print("Pipeline function is running...")
+    
+    weights_dir = setup_psmasegmentator()
+
+    download_fold_weights_via_api(output_dir = weights_dir)
+
+    nnUNet_predict_image(weights_dir, pet_dir, ct_dir, output_dir,
+                         device='cuda', step_size=0.5, use_tta=True, verbose=False)
     
